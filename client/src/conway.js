9edf8f1e03e4bf02ac55f445ee60f2a964792a74
kjohna <43828763+kjohna@users.noreply.github.com>
2019-07-26 07:19:46 -0600

    conway working - probably

--- client/src/conway.js
+++ client/src/conway.js
@@ -1,24 +1,16 @@
 const conway = function(board) {
   // assumes rectangular board
-  // nextBoard = board.map(row => {
-  //   nextRow = row.map(cell => {
-  //     liveNeighbors = countLiveNeighbors(board, r, c);
-  //     console.log(cell);
-  //     return nextCell;
-  //   });
-  //   return nextRow;
-  // });
-  nextBoard = [...board];
-  numRows = nextBoard.length;
-  numCols = nextBoard[0].length;
+  const nextBoard = board.map(row => [...row]); // DON'T DO THIS: [...board];
+  const numRows = nextBoard.length;
+  const numCols = nextBoard[0].length;
   // for each cell
   for (r = 0; r < numRows; r++) {
     for (c = 0; c < numCols; c++) {
       // get live neighbor count
-      liveNeighbors = countLiveNeighbors(board, r, c);
+      const liveNeighbors = countLiveNeighbors(board, r, c);
       // determine if cell lives or dies, populate nextBoard with update
       // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-      cell = board[r][c];
+      const cell = board[r][c];
       if (cell === 1) {
         // Any live cell with fewer than two live neighbours dies
         // Any live cell with two or three live neighbours lives on to the next generation.
@@ -39,39 +31,41 @@ const conway = function(board) {
 
 const countLiveNeighbors = function(board, r, c) {
   // assuming rectangular board
-  return 1;
-  // numRows = board.length;
-  // numCols = board.length;
-  // liveNeighbors = 0;
-  // for (dr = -1; dr < 3; dr++) {
-  //   for (dc = -1; dc < 3; dr++) {
-  //     rowNeighbor = r + dr;
-  //     colNeighbor = c + dc;
-  //     neighbor = 0;
-  //     // only check neighbor for valid indices
-  //     // NOTE: off-board 'neighbors' are dead, no wrap-around
-  //     rowNeighborExists = !(rowNeighbor < 0 || rowNeighbor >= numRows);
-  //     colNeighborExists = !(colNeighbor < 0 || colNeighbor >= numCols);
-  //     notSelf = !(rowNeighbor == r && colNeighbor == c);
-  //     if (rowNeighborExists && colNeighborExists && notSelf) {
-  //       neighbor = board[rNeighbor][colNeighbor];
-  //     }
-  //     if (neighbor == 1) {
-  //       liveNeighbors++;
-  //     }
-  //   }
-  // }
-  // return liveNeighbors;
+  const numRows = board.length;
+  const numCols = board.length;
+  let liveNeighbors = 0;
+  for (let dr = -1; dr < 2; dr++) {
+    for (let dc = -1; dc < 2; dc++) {
+      const rowNeighbor = r + dr;
+      const colNeighbor = c + dc;
+      let neighbor = 0;
+      // only check neighbor for valid indices
+      // NOTE: off-board 'neighbors' are dead, no wrap-around
+      const rowNeighborExists = !(rowNeighbor < 0 || rowNeighbor >= numRows);
+      const colNeighborExists = !(colNeighbor < 0 || colNeighbor >= numCols);
+      const notSelf = !(rowNeighbor == r && colNeighbor == c);
+      if (rowNeighborExists && colNeighborExists && notSelf) {
+        neighbor = board[rowNeighbor][colNeighbor];
+      }
+      if (neighbor == 1) {
+        liveNeighbors++;
+      }
+    }
+  }
+  // console.log(`r, c: ${r},${c} liveNeighbors: ${liveNeighbors}`);
+  return liveNeighbors;
 };
 
 const log_board = function(board) {
   sep = "-".repeat(board.length * 2);
   console.log(sep);
   board.forEach(row => {
+    let printRow = [];
     row.forEach(col => {
-      process.stdout.write(col.toString() + " ");
+      // process.stdout.write(col.toString() + " ");
+      printRow.push(col);
     });
-    console.log("");
+    console.log(printRow.join(" "));
   });
   console.log(sep);
 };
@@ -89,4 +83,13 @@ const board = [
 ];
 
 log_board(board);
-log_board(conway(board));
+let next = conway(board);
+log_board(next);
+// next = conway(next);
+// log_board(conway(next));
+// next = conway(next);
+// log_board(conway(next));
+// next = conway(next);
+// log_board(conway(next));
+// next = conway(next);
+// log_board(conway(next));
