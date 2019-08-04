ec1ba54911e06706ada846ac79cd6e79a840416b
kjohna <43828763+kjohna@users.noreply.github.com>
2019-08-04 17:22:57 -0600

    expo: setup app view

--- client-expo/App.js
+++ client-expo/App.js
@@ -1,10 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import conway from "./conway.js";
 
 export default function App() {
+  const ogBoard = [
+    [1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  // const ogBoard = [
+  //   [0, 0, 0, 0, 0],
+  //   [0, 0, 1, 0, 0],
+  //   [0, 0, 1, 0, 0],
+  //   [0, 0, 1, 0, 0],
+  //   [0, 0, 0, 0, 0]
+  // ];
+  let [board, setBoard] = useState(ogBoard);
+  const [running, setRunning] = useState(false);
+  const [generation, setGeneration] = useState(0);
+  const nextBoard = conway(board);
+  // console.log("here");
+  const [animateBoard, setAnimateBoard] = useState(null);
+
+  useEffect(() => {
+    // console.log("running");
+    if (running) {
+      setBoard(nextBoard);
+    } else {
+      // cancelAnimationFrame(animateBoard);
+      clearTimeout(animateBoard);
+    }
+  }, [running]);
+
+  useEffect(() => {
+    const t = 0.5;
+    // console.log("board");
+    if (running) {
+      setGeneration(prevGeneration => prevGeneration + 1);
+      setAnimateBoard(setTimeout(() => setBoard(nextBoard), t * 1000));
+      // setAnimateBoard(requestAnimationFrame(() => setBoard(nextBoard)));
+    }
+  }, [board]);
+
+  const cellClick = rc => {
+    console.log(`cellClick: ${rc}`);
+    const rcArr = rc.split(",");
+    const r = rcArr[0];
+    const c = rcArr[1];
+    const updatedBoard = board.map(row => [...row]);
+    updatedBoard[r][c] = !board[r][c] ? 1 : 0;
+    setBoard(updatedBoard);
+  };
+
+  const handleReset = () => {
+    setBoard(ogBoard);
+    setGeneration(0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Board board={board} running={running} cellClick={cellClick} /> */}
+      <Button onPress={() => setRunning(!running)} title="Start/Stop" />
+      <Button onPress={() => handleReset()} title="Reset" />
+      <Text>{generation}</Text>
+      <Text>{running && "Running!"}</Text>
     </View>
   );
 }
@@ -12,8 +78,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ client-expo/conway.js
@@ -0,0 +1,84 @@
+export default function(board) {
+  // assumes rectangular board
+  const nextBoard = board.map(row => [...row]); // DON'T DO THIS: [...board];
+  const numRows = nextBoard.length;
+  const numCols = nextBoard[0].length;
+  // for each cell
+  for (let r = 0; r < numRows; r++) {
+    for (let c = 0; c < numCols; c++) {
+      // get live neighbor count
+      const liveNeighbors = countLiveNeighbors(board, r, c);
+      // determine if cell lives or dies, populate nextBoard with update
+      // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+      const cell = board[r][c];
+      if (cell === 1) {
+        // Any live cell with fewer than two live neighbours dies
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        // Any live cell with more than three live neighbours dies
+        if (liveNeighbors < 2 || liveNeighbors > 3) {
+          nextBoard[r][c] = 0;
+        }
+      } else {
+        // Any dead cell with three live neighbours becomes a live cell
+        if (liveNeighbors === 3) {
+          nextBoard[r][c] = 1;
+        }
+      }
+    }
+  }
+  return nextBoard;
+}
+
+const countLiveNeighbors = function(board, r, c) {
+  // assuming rectangular board
+  const numRows = board.length;
+  const numCols = board.length;
+  let liveNeighbors = 0;
+  for (let dr = -1; dr < 2; dr++) {
+    for (let dc = -1; dc < 2; dc++) {
+      const rowNeighbor = r + dr;
+      const colNeighbor = c + dc;
+      let neighbor = 0;
+      // only check neighbor for valid indices
+      // NOTE: off-board 'neighbors' are dead, no wrap-around
+      const rowNeighborExists = !(rowNeighbor < 0 || rowNeighbor >= numRows);
+      const colNeighborExists = !(colNeighbor < 0 || colNeighbor >= numCols);
+      const notSelf = !(rowNeighbor === r && colNeighbor === c);
+      if (rowNeighborExists && colNeighborExists && notSelf) {
+        neighbor = board[rowNeighbor][colNeighbor];
+      }
+      if (neighbor === 1) {
+        liveNeighbors++;
+      }
+    }
+  }
+  // console.log(`r, c: ${r},${c} liveNeighbors: ${liveNeighbors}`);
+  return liveNeighbors;
+};
+
+// // for debugging:
+// const log_board = function(board) {
+//   let sep = "-".repeat(board.length * 2);
+//   console.log(sep);
+//   board.forEach(row => {
+//     let printRow = [];
+//     row.forEach(col => {
+//       // process.stdout.write(col.toString() + " ");
+//       printRow.push(col);
+//     });
+//     console.log(printRow.join(" "));
+//   });
+//   console.log(sep);
+// };
+
+// const board = [
+//   [1, 0, 0, 0, 0, 0, 0, 1, 0],
+//   [0, 1, 1, 0, 0, 0, 0, 1, 0],
+//   [1, 1, 0, 0, 0, 0, 0, 1, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+// ];
